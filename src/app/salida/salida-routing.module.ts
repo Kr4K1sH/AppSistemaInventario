02f2143864f9c6f9cff2056fdc4bc614db99fdb1
BryanElizondo02 <bryanelizondo02@gmail.com>
2021-07-24 16:17:00 -0600

    Routing salida

--- src/app/salida/salida-routing.module.ts
+++ src/app/salida/salida-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SalidaAllComponent } from './salida-all/salida-all.component';
+import { SalidaCreateComponent } from './salida-create/salida-create.component';
 
 const routes: Routes = [
   {path: 'salida/all', component: SalidaAllComponent,},
+  {path: 'salida/create', component: SalidaCreateComponent,},
 
 ];
 
--- src/app/salida/salida.module.ts
+++ src/app/salida/salida.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SalidaAllComponent} from './salida-all/salida-all.component';
 import {SalidaRoutingModule} from './salida-routing.module';
+import { SalidaCreateComponent } from './salida-create/salida-create.component';
 
 @NgModule({
   declarations: [
-    SalidaAllComponent
+    SalidaAllComponent,
+    SalidaCreateComponent
   ],
   imports: [
     CommonModule,
